b7859ccfb26f62558aefbfbbb78df2fd04ed9ca5
Mikey <54429887+MikeyBoi-N@users.noreply.github.com>
2025-09-19 19:17:37 -0400

    blurry top

---
+++ sponsors.js
@@ -0,0 +1,12 @@
+const sponsors = [
+    { name: 'Sponsor 1', image: 'public/sponsors/sponsor1.png', amount: 100 },
+    { name: 'Sponsor 2', image: 'public/sponsors/sponsor2.png', amount: 5000 },
+    { name: 'Sponsor 3', image: 'public/sponsors/sponsor3.png', amount: 1500 },
+    { name: 'Sponsor 4', image: 'public/sponsors/sponsor4.png', amount: 10000 },
+    { name: 'Sponsor 5', image: 'public/sponsors/sponsor5.png', amount: 7500 },
+    { name: 'Sponsor 6', image: 'public/sponsors/sponsor6.png', amount: 250 },
+    { name: 'Sponsor 7', image: 'public/sponsors/sponsor7.png', amount: 800 },
+    { name: 'Sponsor 8', image: 'public/sponsors/sponsor8.png', amount: 2500 },
+    { name: 'Sponsor 9', image: 'public/sponsors/sponsor9.png', amount: 6000 },
+    { name: 'Sponsor 10', image: 'public/sponsors/sponsor10.png', amount: 4000 }
+];
