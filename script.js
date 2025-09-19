e5fb103e6efb0c2765d1848a4d97c235036f1b8a
Mikey <54429887+MikeyBoi-N@users.noreply.github.com>
2025-09-19 13:57:06 -0400

    added stock images

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// Parallax effect removed as per request.
-// Future interactive scripts will be added here.
+const navSlide = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links-mobile');
+
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        hamburger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
