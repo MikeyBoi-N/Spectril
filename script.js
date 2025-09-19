4a917c2664665d2a5287bc5198cac008c82d1aea
Mikey <54429887+MikeyBoi-N@users.noreply.github.com>
2025-09-19 04:01:44 -0400

    site overhaul

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-document.addEventListener('scroll', function() {
-    const hero = document.querySelector('.hero');
-    if (hero) {
-        let offset = window.pageYOffset;
-        hero.style.backgroundPositionY = offset * 0.7 + 'px';
-    }
-});
+// Parallax effect removed as per request.
+// Future interactive scripts will be added here.
